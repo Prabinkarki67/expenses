b87976b33196c73247b2daff828653c5dc989de4
Prabin Karki <161217370+Prabinkarki67@users.noreply.github.com>
2025-09-23 20:01:26 +0545

    worked with models.py

--- expenseTracker/expTracker/models.py
+++ expenseTracker/expTracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+
+class Accounts(models.Model):
+    name = models.CharField(max_length = 100)
+    expense = models.FLoatField(default = 0)
+    user = models.ForeignKey('auth.User', on_delete= models.CASCADE)
+    expense_list = models.ManyToManyField('Expense' )
